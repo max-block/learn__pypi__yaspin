a2be987dd537fc08c27dd38bfe60b66de1ef069d
Max Block <maxblock@pm.me>
2020-11-28 12:23:15 +0300

    update

--- t0__test.py
+++ t0__test.py
@@ -1,14 +1,13 @@
-import time
-
 from yaspin import yaspin
 
 with yaspin() as sp:
-    sp.text = "processed 1"
-    time.sleep(1)
-    sp.text = "processed 2"
-    time.sleep(1)
-    sp.text = "processed 3"
-    time.sleep(1)
-    sp.text = "processed 4"
+    sp.text = "fff"
+    # sp.text = "processed 1"
+    # time.sleep(1)
+    # sp.text = "processed 2"
+    # time.sleep(1)
+    # sp.text = "processed 3"
+    # time.sleep(1)
+    # sp.text = "processed 4"
 
 print("done!")
